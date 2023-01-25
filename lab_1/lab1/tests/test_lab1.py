a35ae72a1ad52b6ed857437eab4b337a1a32fae4
lglizaran <lglizaran@gmail.com>
2023-01-25 19:46:43 +0000

    Add tests

--- lab_1/lab1/tests/test_lab1.py
+++ lab_1/lab1/tests/test_lab1.py
@@ -6,5 +6,25 @@ client = TestClient(app)
 
 def test_read_main():
     response = client.get("/") # get the root endpoint
-    assert response.status_code == 200 # check response status is 200
-    assert response.json() == {"message": "Hello World"} # check response
+    assert response.status_code == 501 # check response status is 501
+    assert response.json() == {'detail': 'Not Implemented'} # check response
+
+def test_hello():
+    response = client.get("/hello/", params={"name": "foo"})
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello foo"}
+
+def test_hello_bad():
+    response = client.get("/hello/")
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Name must be specified, please type /home/?name={name}'}
+
+def test_docs():
+    response = client.get("/docs/")
+    assert response.status_code == 200
+    # assert response.json() == {"message": "Hello foo"}
+
+def test_openapi_json():
+    response = client.get("/openapi.json")
+    assert response.status_code == 200
+    # assert response.json() == {"message": "Hello foo"}
