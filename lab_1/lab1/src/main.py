e718ee03e008e2d28d4ca18e655390df6ab9c403
lglizaran <lglizaran@gmail.com>
2023-01-29 21:17:53 +0000

    Update pyteest with response

--- lab_1/lab1/src/main.py
+++ lab_1/lab1/src/main.py
@@ -9,7 +9,7 @@ async def read_main():
 @app.get("/hello")
 async def read_name(name: str | None = None):
     if not name:
-        raise HTTPException(status_code=400, detail="Name must be specified, please type /hello/?name={name}")
+        raise HTTPException(status_code=400, detail="Name must be specified, please type /hello?name={name}")
     return {"message": f"Hello {name}"}
 
 # health check to assure app is up
